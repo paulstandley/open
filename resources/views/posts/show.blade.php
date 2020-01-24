2eb856eca6e8000af85baa920771ab675a706fa9
Paul Standley <paulstandley1972@gmail.com>
2020-01-24 21:36:41 +0000

    posts show

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,6 +2,38 @@
 
 @section('content')
 <div class="container">
-  <h2>posts show</h2>
+  <div class="row post_section p-4">
+    <div class="col-8">
+      <img class="w-100" src="/storage/{{  $post->image  }}" alt="{{  $post->caption  }}">
+    </div>
+    <div class="col-4">
+      <div class="d-flex align-items-center">
+        <div class="pr-3">
+          <img src="{{  $post->user->profile->profileImage()  }}" alt="profile pick" class="rounded-circle w-100" style="max-width: 50px;">
+        </div>
+        <div>
+          <div class="font-weight-bold">
+            <a href="/profile/{{  $post->user->id  }}">
+              <span class="text-dark">
+                {{  $post->user->username  }}
+              </span>
+            </a>
+            <a href="/profile/{{   $post->user->id  }}" class="pl-2">Follow</a>
+          </div>
+        </div>
+      </div>
+      <hr>
+      <p>
+        <span class="font-weight-bold">
+          <a href="/profile/{{  $post->user->id  }}">
+            <span class="text-dark">
+              {{  $post->user->username  }}
+            </span>
+          </a>
+        </span>
+        {{  $post->caption  }}
+      </p>
+    </div>
+  </div>
 </div>
 @endsection
