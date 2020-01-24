2b40b6e4b52e4a5285534bfd978c276085c4b821
Paul Standley <paulstandley1972@gmail.com>
2020-01-24 16:58:25 +0000

    routes

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,21 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Use App\Mail\NewUserWelcomeMail;
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/email', function() {
+  return new NewUserWelcomeMail();
+});
+
+Route::post('follow/{user}', 'FollowsController@store');
+
+Route::get('/', 'PostsController@index');
+Route::get('/p/create', 'PostsController@create');
+Route::post('p', 'PostsController@store');
+Route::get('/p/{post}', 'PostsController@show');
+
+Route::get('/profile/{user}', 'ProfilesController@index')->name('profile.show');
+Route::get('/profile/{user}/edit', 'ProfilesController@edit')->name('profile.edit');
+Route::patch('/profile/{user}', 'ProfilesController@update')->name('profile.update');
