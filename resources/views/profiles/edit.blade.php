9f2037c6eac0a7af695c388422929f714fe16eef
Paul Standley <paulstandley1972@gmail.com>
2020-01-24 21:40:49 +0000

    profile edit

--- resources/views/profiles/edit.blade.php
+++ resources/views/profiles/edit.blade.php
@@ -2,6 +2,81 @@
 
 @section('content')
 <div class="container">
-  <h2>profile edit</h2>
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+        <div class="card ">
+            <div class="card-header text-center font-italic">{{ __('Edit Profle') }}</div>
+
+            <div class="card-body justify">
+                <form method="POST" action="/profile/{{  $user->id  }}" accept="/p" enctype="multipart/form-data">
+                    @csrf
+                    @method('PATCH')
+                    <div class="form-group row">
+                        <label for="title" class="col-md-4 col-form-label text-md-right">{{ __('Title') }}</label>
+
+                        <div class="col-md-6">
+                            <input id="title" type="text" class="form-control @error('title') is-invalid @enderror" name="title" value="{{ old('title') ?? $user->profile->title }}" required autocomplete="title" autofocus>
+
+                            @error('title')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                      <label for="description" class="col-md-4 col-form-label text-md-right">{{ __('Description') }}</label>
+
+                      <div class="col-md-6">
+                          <input id="description" type="text" class="form-control @error('description') is-invalid @enderror" name="description" value="{{ old('description') ?? $user->profile->description }}" required autocomplete="description" autofocus>
+
+                          @error('description')
+                              <span class="invalid-feedback" role="alert">
+                                  <strong>{{ $message }}</strong>
+                              </span>
+                          @enderror
+                      </div>
+                  </div>
+
+                  <div class="form-group row">
+                    <label for="url" class="col-md-4 col-form-label text-md-right">{{ __('Url') }}</label>
+
+                    <div class="col-md-6">
+                        <input id="url" type="text" class="form-control @error('url') is-invalid @enderror" name="url" value="{{ old('url') ?? $user->profile->url }}" required autocomplete="url" autofocus>
+
+                        @error('url')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                </div>
+
+                    <div class="form-group row">
+                        <label for="image" class="col-md-4 col-form-label text-md-right">{{ __('Profile Image') }}</label>
+
+                        <div class="col-md-6">
+                            <input id="image" type="file" class="form-control @error('image') is-invalid @enderror" name="image" value="{{ old('image') }}" autocomplete="image">
+
+                            @error('image')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="form-group row mb-0">
+                      <div class="col-md-6 offset-md-4">
+                          <button type="submit" class="btn btn-primary">
+                              {{ __('Save Profile') }}
+                          </button>
+                      </div>
+                  </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
 </div>
 @endsection
