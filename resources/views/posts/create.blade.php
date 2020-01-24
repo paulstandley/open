ffdf1d65731e91f717ce6a04f41b6b8f8614da0f
Paul Standley <paulstandley1972@gmail.com>
2020-01-24 21:28:45 +0000

    posts create

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,6 +2,53 @@
 
 @section('content')
 <div class="container">
-  <h2>post create</h2>
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+        <div class="card ">
+            <div class="card-header text-center font-italic">{{ __('Post') }}</div>
+
+            <div class="card-body justify">
+                <form method="POST" action="/p" accept="/p" enctype="multipart/form-data">
+                    @csrf
+
+                    <div class="form-group row">
+                        <label for="caption" class="col-md-4 col-form-label text-md-right">{{ __('Caption') }}</label>
+
+                        <div class="col-md-6">
+                            <input id="caption" type="text" class="form-control @error('caption') is-invalid @enderror" name="caption" value="{{ old('caption') }}" required autocomplete="caption" autofocus>
+
+                            @error('caption')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <label for="image" class="col-md-4 col-form-label text-md-right">{{ __('Image') }}</label>
+
+                        <div class="col-md-6">
+                            <input id="image" type="file" class="form-control @error('image') is-invalid @enderror" name="image" value="{{ old('image') }}" required autocomplete="image">
+
+                            @error('image')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="form-group row mb-0">
+                      <div class="col-md-6 offset-md-4">
+                          <button type="submit" class="btn btn-primary">
+                              {{ __('Add post') }}
+                          </button>
+                      </div>
+                  </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
 </div>
 @endsection
