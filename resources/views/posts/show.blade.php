f6948cd06b0a11716754c71c207fc356c912a6b3
Paul Standley <paulstandley1972@gmail.com>
2020-01-24 17:58:33 +0000

    add view

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <h2>posts show</h2>
+</div>
+@endsection
---
+++ resources/views/profiles/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <h2>Profile index</h2>
+</div>
+@endsection
